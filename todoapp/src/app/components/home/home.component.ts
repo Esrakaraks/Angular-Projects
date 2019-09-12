54b8c232b4ce7da8eba1badb18d2eb09fe62b60e
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-09-12 10:00:24 +0300

    update

--- todoapp/src/app/components/home/home.component.ts
+++ todoapp/src/app/components/home/home.component.ts
@@ -1,34 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { callbackify } from 'util';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  Pendings = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep'
-  ];
+  data={
 
-  inProgress = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
-  ];
-  done = [
-    'Get up',
-    'Brush teeth',
-    'Walk dog'
-  ];
+    Pendings : [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep'
+    ],
+  
+    inProgress : [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ],
+    done : [
+      'Get up',
+      'Brush teeth',
+      'Walk dog'
+    ]
+}
+ 
   constructor() { }
 
   ngOnInit() {
+    this.setItems();
   }
+
+
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
@@ -39,5 +47,27 @@ export class HomeComponent implements OnInit {
                         event.currentIndex);
     }
   }
+
+  setItems(){
+      Object.keys(this.data).forEach(callbackfn:(key) => {
+        if(!localStorage.getItem(key)){
+          localStorage.setItem(key,JSON.stringify(this.data[key]));
+        
+        }
+        else{
+          this.data[key]=JSON.parse(localStorage.getItem(key))
+        }
+
+      });
   }
+  
+  
+  addTodo(todo){
+    this.data.Pendings.push(todo.value);
+    todo.value='';
+
+  }
+
+  }
+  
 
