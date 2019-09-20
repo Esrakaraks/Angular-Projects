d08e3dcd67ee703a0c5bacda73e003bdbfd7f26b
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-09-20 09:48:40 +0300

    update

--- movieApp/src/app/app.module.ts
+++ movieApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './moviesComponents/movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
