37f58bf68c4cc554ed0488d3dc76293010b36909
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-09-20 16:36:31 +0300

    update

---
+++ movieApp/src/app/moviesComponents/movies/movieData.ts
@@ -0,0 +1,13 @@
+export class MovieData {
+  id:number;
+  name:string
+  items=[
+    {id :1 ,name :"Harry Potter"},
+    {id :2 ,name :"Avengers"}
+   
+  ]
+}
+  
+   
+   
+  
--- movieApp/src/app/moviesComponents/movies/movies.component.ts
+++ movieApp/src/app/moviesComponents/movies/movies.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {MovieData} from './movieData';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
-export class MoviesComponent implements OnInit {
+export class MoviesComponent {
+     title= "movie App";
+     movies=new MovieData ;
+ getItems(){
+   return this.movies.items;
+ }
+  
+ 
+  
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+   
+ 
 }
