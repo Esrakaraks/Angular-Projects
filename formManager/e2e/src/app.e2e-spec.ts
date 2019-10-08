6fb967563cb79cd26f54af3b124b2c2191d34df9
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-10-08 18:35:52 +0300

    update

--- formManager/e2e/src/app.e2e-spec.ts
+++ formManager/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('FormList app is running!');
+    expect(page.getTitleText()).toEqual('formManager app is running!');
   });
 
   afterEach(async () => {
--- formManager/src/app/app.component.spec.ts
+++ formManager/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FormList'`, () => {
+  it(`should have as title 'formManager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('FormList');
+    expect(app.title).toEqual('formManager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('FormList app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('formManager app is running!');
   });
 });
