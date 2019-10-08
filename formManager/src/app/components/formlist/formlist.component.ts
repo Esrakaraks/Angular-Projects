6eccbc8bc324a38f3a20fcca289594c0a7b2b3ab
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-10-08 23:50:23 +0300

    update

---
+++ formManager/src/app/components/formlist/formlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formlist',
+  templateUrl: './formlist.component.html',
+  styleUrls: ['./formlist.component.css']
+})
+export class FormlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
