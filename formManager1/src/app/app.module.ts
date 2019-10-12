7bbcbdb2506ab06709873cf2276dbce0c77880da
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-10-12 15:41:40 +0300

    update

--- formManager1/src/app/app.module.ts
+++ formManager1/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
@@ -16,7 +16,7 @@ import { FormListComponent } from './components/form-list/form-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
